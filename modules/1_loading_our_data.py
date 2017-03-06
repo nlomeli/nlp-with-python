52d55dca7cfa9a14d4a1938419d2782661bccc8a
nlomeli <nathan.lomeli@gmail.com>
2017-03-05 16:41:25 -0800

    exploring our corpus

--- modules/1_loading_our_data.py
+++ modules/1_loading_our_data.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+import random
 
 try:
     directory = sys.args[1]
@@ -14,18 +15,49 @@ reviews = reviews[5:] # get rid of 5 shortest reviews
 for r in reviews:
     r.pop('by') # remove 'by' field in each review
 
+reviews.sort(key=lambda r:r['release_year']) # sort by release_year
+
+titles = {r['title']:r for r in reviews}
+
+def display(r):
+    print("""
+    Review: %s
+    ------------------------------
+    Title: %s
+    Release year: %s
+    Running time: %s
+    MPAA rating: %s
+    Genres: %s
+    Star rating: %s
+    Date Published: %s
+    Review URL: %s
+    """ % (
+    r['text'],r['title'],r['release_year'],r['running_time'],r['mpaa_rating'],
+    ', '.join(r['genres']) if r['genres'] else 'N/A',
+    r['star_rating'],r['date_published'],r['url']
+    ))
+
 ####
 
-# inspect data
-# look at breakdown of corpus
-# quantity of movies by mpaa_rating, star_rating, etc.
-# read some text excerpts
+type(reviews) # list
+reviews
+len(reviews) # 7734
+type(reviews[0]) # dict
+reviews[0] # oldest movie by release year
+reviews[-1] # newest movie by release year
+random.choice(reviews)
+r = random.choice(reviews)
+sorted(r) # review object fields
+r # review object
+r['title']
+print(r['text'])
+display(r) # custom printed view
+display(random.choice(reviews))
+
+# read some excerpts
 # visit a couple links
-# write a function to search for a movie review by title:
-#   search_string.lower() in review['title']
 
-
-
+# let's write our own movie search function
 def search(q, reviews):
     matches = {}
     for r in reviews:
@@ -35,9 +67,13 @@ def search(q, reviews):
     return matches
 
 results = search('war', reviews)
-sorted(results)
+sorted(results) # alphabetical list of results titles
 results['The Fog of War (2004)']
 
-titles = {r['title']:r for r in reviews}
-# >>> titles['Minor[TAB] for autocompletion
-titles['Minority Report (2002)']
+type(titles) # dictionary of reviews by title
+titles
+len(titles) # yup, 7734 of them
+sorted(titles)
+# >>> titles['My Movi... [TAB] for autocompletion
+mr = titles['Minority Report (2002)'] # titles['Minor[TAB]...
+display(mr)
