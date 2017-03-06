e20670bf306803a1597f4b474cc93d4f02665021
nlomeli <nathan.lomeli@gmail.com>
2017-03-06 12:07:16 -0800

    revised 05

--- modules/05_spacy.py
+++ modules/05_spacy.py
@@ -29,35 +29,61 @@ nlp = spacy.load('en') # takes time
 mr = titles['Minority Report (2002)']
 mr_text = mr['text']
 mr_doc = nlp(mr_text)
+type(mr_doc) # spacy.tokens.doc.Doc
+mr_doc.text
+list(mr_doc.sents)
+len(list(mr_doc.sents)) # 49
+list(mr_doc)
+len(mr_doc) # 1376 tokens
 list(mr_doc)[:30]
+type(mr_doc[0]) # spacy.tokens.token.Token
+token = mr_doc[0]
+# dependency parser visualization
+# https://demos.explosion.ai/displacy/
+# enter:
+# At a time when movies think they have to choose between action and ideas, Steven Spielberg's "Minority Report" is a triumph--a film that works on our minds and our emotions.
 
+# token.[TAB] to see ways to navigate the grammatical tree of the sentence
+token.pos_
+token.head # is
+list(token.children) # time
+token.lemma # 507--a unique identifier for this word in the spaCy English vocab
+
+# explore other sentences in the dependency visualizer
+
+# let's break down nlp() into its component parts
 mr_doc = nlp.tokenizer(mr_text) # parses into spacy.tokens.token.Token objects
 nlp.tagger(mr_doc) # pos tagger
 nlp.parser(mr_doc) # dependency parser
 nlp.entity(mr_doc) # entity recongizer
 
-# dependency parser visualization
-# https://demos.explosion.ai/displacy/
+# lots of processing is done with a simple call to nlp()
+list((tok.text, tok.pos_) for tok in mr_doc) # tokens with their pos
+list(mr_doc.noun_chunks)
+for sent in mr_doc.sents:
+    print(sent.orth_)
+    print(sent.root)
+    print('')
+mr_doc.ents
 
-# show sentences parsed into trees with verbs as base of trees
-# grab those verbs out
-# maybe groub out subject-relation-direct object triples
+# built-in entity types
+# https://spacy.io/docs/usage/entity-recognition
+entity = mr_doc.ents[0]
+entity.orth_ # orthographic representation "Steven Spielberg's"
+entity.label_ # 'PERSON'
+entities = [e for e in mr_doc.ents]
+people = [e for e in entities if e.label_=='PERSON']
+artworks = [e for e in entities if e.label_=='WORK_OF_ART']
+dates = [e for e in entities if e.label_=='DATE'] # error: 'mid-air'
 
-# doc1.similarity(doc2)
+# Named Entity Recognition (NER) visualization
+# https://demos.explosion.ai/displacy-ent/
 
-# list((w.text, w.pos_) for w in mr_doc)
-
-# Multi-threading with .pipe()
-# https://spacy.io/docs/usage/processing-text#multithreading
-texts = [r['text'] for r in reviews]
-for i, doc in enumerate(nlp.pipe(texts, batch_size=10000, n_threads=3)):
-    reviews[i]['doc'] = doc
-
-
-
-for r in reviews:
-    print(r['title'])
-    docs.append(nlp(r['text']))
+# let's process multiple docs
+# doing all of them will take forever:
+#for r in reviews:
+#    print(r['title'])
+#    docs.append(nlp(r['text']))
 
 g_rated = [r for r in reviews if r['mpaa_rating']=='G']
 len(g_rated)
@@ -74,17 +100,11 @@ g_rated[-3]['title'] # Beauty And The Beast
 g_rated[0]['title'] # Stormchasers
 g_rated[1]['title'] # Microcosmos
 g_rated[2]['title'] # Hubble 3D
-# or
+
+# let's see everything
 mermaid_like = [(mermaid_doc.similarity(r['doc']),r) for r in g_rated]
 for sim_score, r in mermaid_like:
     print(sim_score, r['title'])
-
-s = list(mermaid_docs.sents)[3]
-s.root
-list(s.root.children)
-
-for sent in mermaid_doc.sents:
-    print(sent.root)
 
 # semantic similarity visualization
 # https://demos.explosion.ai/sense2vec/?word=natural%20language%20processing&sense=auto
@@ -93,10 +113,10 @@ for sent in mermaid_doc.sents:
 # with matrix, could find most similar/disimilar pairs in a bunch
 # could collect disaster movies (23) to find these matches
 
+# named entities:
 # pull out different types of named entities
 # aggregate named entities across reviews
 # look at aggregates of different types of named entities across corpus
 
-
-# NER visualization
-# https://demos.explosion.ai/displacy-ent/
+# knowledge extraction
+# extract groups of subject-relation-direct object triples
